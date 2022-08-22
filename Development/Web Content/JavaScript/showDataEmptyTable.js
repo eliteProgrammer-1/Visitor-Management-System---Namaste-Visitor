6503deb147688feac98b0970a851883fb2e21bad
Manish Sharma <sharmamanish16111999@gmail.com>
2022-08-22 13:01:45 +0530

    changes heading tag of bootstrap alert

--- Development/Web Content/JavaScript/showDataEmptyTable.js
+++ Development/Web Content/JavaScript/showDataEmptyTable.js
@@ -3,9 +3,11 @@ window.onload = function()
     if((tableBody.children).length == 0)
     {
         document.getElementById("tableBoxOfShowEntries").innerHTML = `<div class="alert alert-warning" role="alert" style="width: 100%; display: flex; justify-content: center; align-items: center; flex-direction: column;">
-        <h4 class="alert-heading">Oops!</h4>
+        <h2 class="alert-heading">Oops!</h2>
         <hr>
         <p class="mb-0">No Data Found.</p>
-    </div>`;    
+    </div>`;  // bootstrap alert
+    
+    document.getElementById("sortData").style.display = "none"; // sorting functionality is not needed
     }
 }
